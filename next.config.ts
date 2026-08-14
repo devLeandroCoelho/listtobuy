ccde98ae4f68836270281d677db9d53901203dc3
Leandro Prazeres Coelho <devleandrocoelho@gmail.com>
2026-08-14 12:45:56 -0300

    feat(#124): instalar/configurar next-pwa com service worker e cache

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import withPWA from '@ducanh2912/next-pwa';
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -8,4 +9,22 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const pwaConfig = {
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+} as any;
+
+export default withPWA(pwaConfig)(nextConfig);
